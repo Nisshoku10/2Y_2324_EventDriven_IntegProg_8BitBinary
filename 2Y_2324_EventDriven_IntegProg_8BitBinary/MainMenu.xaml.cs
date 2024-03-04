06c092a51f3d06b9bc108d2ff29e373906cb733a
23-0179c <23-0179c@CCL01-25>
2024-03-04 17:17:36 +0800

    Add project files.

---
+++ 2Y_2324_EventDriven_IntegProg_8BitBinary/MainMenu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _2Y_2324_EventDriven_IntegProg_8BitBinary
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
